2892fa882d163173ceca6a6a1aeb192734fc0c8e
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-10-27 15:00:57 -0400

    feat(helpers): Update types.py
    Create subsection dividers.
    Add 2 type aliases.

--- etlplus/types.py
+++ etlplus/types.py
@@ -8,12 +8,23 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import Any
+from typing import Callable
 
 
 # SECTION: TYPE ALIASES ===================================================== #
 
 
+# -- Data -- #
+
 type JSONDict = dict[str, Any]
 type JSONList = list[JSONDict]
 type JSONData = JSONDict | JSONList
+
+# -- File System -- #
+
 type StrPath = str | Path
+
+# -- Functions -- #
+
+type OperatorFunc = Callable[[Any, Any], bool]
+type AggregateFunc = Callable[[list[float], int], Any]
