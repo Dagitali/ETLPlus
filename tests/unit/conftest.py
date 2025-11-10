08b8015d6580c2bb6b1cfe8ea0e5f0cdc2a1b89a
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-11-10 15:01:58 -0500

    test(unit): Create conftest.py

---
+++ tests/unit/conftest.py
@@ -0,0 +1,9 @@
+"""
+tests.unit.conftest pytest configuration module.
+
+Configures pytest-based unit tests.
+"""
+import pytest
+
+# Mark all tests in this directory as unit tests
+pytestmark = pytest.mark.unit
