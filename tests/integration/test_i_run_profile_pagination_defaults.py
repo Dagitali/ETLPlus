1f5ef1fd5a41e92085fe0e35f9139665e956a3c7
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-11-24 00:10:29 -0500

    test(api): Update test_i_run_profile_pagination_defaults.py
    Revert from using relative import to absolute import.

--- tests/integration/test_i_run_profile_pagination_defaults.py
+++ tests/integration/test_i_run_profile_pagination_defaults.py
@@ -16,9 +16,9 @@ from __future__ import annotations
 from typing import Any
 from typing import Callable
 
-from .conftest import FakeEndpointClientProtocol as Client
 from etlplus.config import PaginationConfig
 from etlplus.config import PipelineConfig
+from tests.integration.conftest import FakeEndpointClientProtocol as Client
 
 
 # SECTION: TESTS ============================================================ #
