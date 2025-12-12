6806486feca1444cfa63ed3112b7e25076d1e753
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-12-12 14:19:17 -0500

    test(integration): Update conftest.py
    Change import source of PaginationConfig.
    - etlplus.config -> etlplus.api

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -17,6 +17,7 @@ from typing import Protocol
 
 import pytest
 
+from etlplus.api import PaginationConfig
 from etlplus.api import RateLimitConfig
 from etlplus.api import RateLimiter
 from etlplus.config import ApiConfig
@@ -27,7 +28,6 @@ from etlplus.config import EndpointConfig
 from etlplus.config import ExtractRef
 from etlplus.config import JobConfig
 from etlplus.config import LoadRef
-from etlplus.config import PaginationConfig
 from etlplus.config import PipelineConfig
 
 # SECTION: HELPERS ========================================================== #
