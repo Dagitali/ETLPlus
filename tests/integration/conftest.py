93b04676c63c887f46dc32c07caff5f6d08458dd
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-12-13 11:45:54 -0500

    test(integration): Update conftest.py
    Change import source for 3 dataclass.
    - ApiConfig: etlplus.config -> etlplus.api
    - ApiProfileConfig: etlplus.config -> etlplus.api
    - EndpointConfig: etlplus.config -> etlplus.api

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -17,14 +17,14 @@ from typing import Protocol
 
 import pytest
 
+from etlplus.api import ApiConfig
+from etlplus.api import ApiProfileConfig
+from etlplus.api import EndpointConfig
 from etlplus.api import PaginationConfig
 from etlplus.api import RateLimitConfig
 from etlplus.api import RateLimiter
-from etlplus.config import ApiConfig
-from etlplus.config import ApiProfileConfig
 from etlplus.config import ConnectorApi
 from etlplus.config import ConnectorFile
-from etlplus.config import EndpointConfig
 from etlplus.config import ExtractRef
 from etlplus.config import JobConfig
 from etlplus.config import LoadRef
