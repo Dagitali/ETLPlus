582c4628526c7f83e81beee000260620bf47543d
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-12-17 15:34:23 -0500

    ci(project): Update pyproject.toml, setup.py, __version__.py
    Bump up project version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.md', encoding='utf-8') as fh:
 
 setup(
     name='etlplus',
-    version='0.3.1',
+    version='0.3.2',
     author='ETLPlus Team',
     description='A Swiss Army knife for enabling simple ETL operations',
     long_description=long_description,
