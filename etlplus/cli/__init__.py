1d1da005f56501f14e21bfd5965ef3ee698b59ef
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-02 09:37:27 -0500

    feat(cli): Create __init__.py

---
+++ etlplus/cli/__init__.py
@@ -0,0 +1,16 @@
+"""
+:mod:`etlplus.cli` package.
+
+This package defines the main command-line interface (CLI) command and
+subcommands for ``etlplus``.
+
+"""
+
+from __future__ import annotations
+
+from .cli import main
+
+# SECTION: EXPORTS ========================================================== #
+
+
+__all__ = ['main']
