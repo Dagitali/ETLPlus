eae16695fadcd30a58e074ac9748da45bab5863e
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-12 14:42:29 -0500

    feat(file): Create __init__.py

---
+++ etlplus/file/__init__.py
@@ -0,0 +1,14 @@
+"""
+:mod:`etlplus.file` package.
+
+Public file IO helpers.
+"""
+
+from __future__ import annotations
+
+from .core import File
+
+# SECTION: EXPORTS ========================================================== #
+
+
+__all__ = ['File']
