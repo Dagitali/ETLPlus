262fd27f1bafbe490fda048d77646eb00b3fba15
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-25 18:22:37 -0500

    docs(file): Update sav.py

--- etlplus/file/sav.py
+++ etlplus/file/sav.py
@@ -27,6 +27,7 @@ from . import stub
 
 
 __all__ = [
+    # Functions
     'read',
     'write',
 ]
@@ -39,7 +40,7 @@ def read(
     path: Path,
 ) -> JSONList:
     """
-    Read SAV content from ``path``.
+    Read SAV content from *path*.
 
     Parameters
     ----------
@@ -59,7 +60,7 @@ def write(
     data: JSONData,
 ) -> int:
     """
-    Write ``data`` to SAV file at ``path`` and return record count.
+    Write *data* to SAV file at *path* and return record count.
 
     Parameters
     ----------
