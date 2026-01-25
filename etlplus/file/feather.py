a6746d476632e918c2f17eb5369f8d7881f160ae
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-25 18:06:52 -0500

    docs(file): Update feather.py

--- etlplus/file/feather.py
+++ etlplus/file/feather.py
@@ -30,6 +30,7 @@ from ._io import normalize_records
 
 
 __all__ = [
+    # Functions
     'read',
     'write',
 ]
@@ -42,7 +43,7 @@ def read(
     path: Path,
 ) -> JSONList:
     """
-    Read Feather content from ``path``.
+    Read Feather content from *path*.
 
     Parameters
     ----------
@@ -75,7 +76,7 @@ def write(
     data: JSONData,
 ) -> int:
     """
-    Write ``data`` to Feather at ``path`` and return record count.
+    Write *data* to Feather at *path* and return record count.
 
     Parameters
     ----------
