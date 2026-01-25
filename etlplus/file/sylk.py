3b75647dba3940a43dd00c745cfbdb2106e768c7
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-25 18:23:59 -0500

    docs(file): Update sylk.py

--- etlplus/file/sylk.py
+++ etlplus/file/sylk.py
@@ -27,6 +27,7 @@ from . import stub
 
 
 __all__ = [
+    # Functions
     'read',
     'write',
 ]
@@ -39,7 +40,7 @@ def read(
     path: Path,
 ) -> JSONList:
     """
-    Read SYLK content from ``path``.
+    Read SYLK content from *path*.
 
     Parameters
     ----------
@@ -59,7 +60,7 @@ def write(
     data: JSONData,
 ) -> int:
     """
-    Write ``data`` to SYLK file at ``path`` and return record count.
+    Write *data* to SYLK file at *path* and return record count.
 
     Parameters
     ----------
