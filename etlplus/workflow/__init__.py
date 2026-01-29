7e4cbbbc193aa34c2adba9e296d57e49e5ed6b37
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-28 20:01:00 -0500

    revert(workflow): Update __init__.py

--- etlplus/workflow/__init__.py
+++ etlplus/workflow/__init__.py
@@ -6,11 +6,6 @@ Job workflow helpers.
 
 from __future__ import annotations
 
-from .connector import Connector
-from .connector import ConnectorApi
-from .connector import ConnectorDb
-from .connector import ConnectorFile
-from .connector import parse_connector
 from .dag import topological_sort_jobs
 from .jobs import ExtractRef
 from .jobs import JobConfig
@@ -25,9 +20,6 @@ from .pipeline import load_pipeline_config
 
 __all__ = [
     # Data Classes
-    'ConnectorApi',
-    'ConnectorDb',
-    'ConnectorFile',
     'ExtractRef',
     'JobConfig',
     'LoadRef',
@@ -36,8 +28,5 @@ __all__ = [
     'ValidationRef',
     # Functions
     'load_pipeline_config',
-    'parse_connector',
     'topological_sort_jobs',
-    # Type Aliases
-    'Connector',
 ]
