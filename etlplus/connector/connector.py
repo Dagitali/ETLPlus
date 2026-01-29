e97137aa61e3c4fec12f281a6ef59c643ceaaecd
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-01-29 12:38:32 -0500

    refactor(connectors): Move connector.py
    ...from etlplus/connectors/.
    ...to etlplus/connector/.
    Source data classes from imports.

---
+++ etlplus/connector/connector.py
@@ -0,0 +1,33 @@
+"""
+:mod:`etlplus.connector.connector` module.
+
+Compatibility re-exports for connector configuration classes.
+"""
+
+from __future__ import annotations
+
+from .api import ConnectorApi
+from .database import ConnectorDb
+from .file import ConnectorFile
+from .utils import parse_connector
+
+# SECTION: EXPORTS ========================================================== #
+
+
+__all__ = [
+    # Data Classes
+    'ConnectorApi',
+    'ConnectorDb',
+    'ConnectorFile',
+    # Functions
+    'parse_connector',
+    # Type aliases
+    'Connector',
+]
+
+
+# SECTION: TYPED ALIASES ==================================================== #
+
+
+# Type alias representing any supported connector
+type Connector = ConnectorApi | ConnectorDb | ConnectorFile
