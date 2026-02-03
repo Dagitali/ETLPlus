4a5c5f68c63ee2f88fe63481f7713a757a065cef
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-03 16:37:40 -0500

    refactor(file): Update tab.py

--- etlplus/file/tab.py
+++ etlplus/file/tab.py
@@ -79,4 +79,4 @@ def write(
     int
         The number of rows written to the TAB file.
     """
-    return write_delimited(path, data, delimiter='\t')
+    return write_delimited(path, data, delimiter='\t', format_name='TAB')
