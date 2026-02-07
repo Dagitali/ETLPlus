90aec9ed73533b05d20f84a90faedd36eba5bf83
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-07 15:51:54 -0500

    refactor(file): Update tsv.py

--- etlplus/file/tsv.py
+++ etlplus/file/tsv.py
@@ -132,10 +132,10 @@ class TsvFile(DelimitedTextFileHandlerABC):
         JSONList
             Parsed rows.
         """
-        delimiter = self.delimiter
-        if options is not None:
-            delimiter = str(options.extras.get('delimiter', delimiter))
-        return read_delimited(path, delimiter=delimiter)
+        return read_delimited(
+            path,
+            delimiter=self.delimiter_from_read_options(options),
+        )
 
     def write_rows(
         self,
@@ -162,13 +162,10 @@ class TsvFile(DelimitedTextFileHandlerABC):
         int
             The number of rows written to the TSV file.
         """
-        delimiter = self.delimiter
-        if options is not None:
-            delimiter = str(options.extras.get('delimiter', delimiter))
         return write_delimited(
             path,
             rows,
-            delimiter=delimiter,
+            delimiter=self.delimiter_from_write_options(options),
             format_name='TSV',
         )
 
