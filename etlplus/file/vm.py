ae68b739a7456ef514ae19e25fbea7f8c9d74889
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-07 08:34:51 -0500

    feat(file): Update vm.py

--- etlplus/file/vm.py
+++ etlplus/file/vm.py
@@ -19,20 +19,61 @@ Notes
 
 from __future__ import annotations
 
+from pathlib import Path
+
 from ..types import JSONData
 from ..types import JSONList
 from ..types import StrPath
-from . import stub
 from ._io import coerce_path
+from .base import ReadOptions
+from .base import WriteOptions
+from .enums import FileFormat
+from .stub import StubFileHandlerABC
 
 # SECTION: EXPORTS ========================================================== #
 
 
 __all__ = [
+    # Classes
+    'VmFile',
     # Functions
     'read',
     'write',
 ]
+
+
+# SECTION: CLASSES ========================================================== #
+
+
+class VmFile(StubFileHandlerABC):
+    """
+    Stub handler implementation for VM files.
+    """
+
+    format = FileFormat.VM
+
+    def read(
+        self,
+        path: Path,
+        *,
+        options: ReadOptions | None = None,
+    ) -> JSONList:
+        return super().read(path, options=options)
+
+    def write(
+        self,
+        path: Path,
+        data: JSONData,
+        *,
+        options: WriteOptions | None = None,
+    ) -> int:
+        return super().write(path, data, options=options)
+
+
+# SECTION: INTERNAL CONSTANTS ============================================== #
+
+
+_VM_HANDLER = VmFile()
 
 
 # SECTION: FUNCTIONS ======================================================== #
@@ -54,7 +95,7 @@ def read(
     JSONList
         The list of dictionaries read from the VM file.
     """
-    return stub.read(path, format_name='VM')
+    return _VM_HANDLER.read(coerce_path(path))
 
 
 def write(
@@ -77,5 +118,4 @@ def write(
     int
         The number of rows written to the VM file.
     """
-    path = coerce_path(path)
-    return stub.write(path, data, format_name='VM')
+    return _VM_HANDLER.write(coerce_path(path), data)
