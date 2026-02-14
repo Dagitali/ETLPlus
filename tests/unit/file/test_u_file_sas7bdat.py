651fe2ff671f67ead486e18aee92865f0b5810ba
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-14 11:16:20 -0500

    refactor: Update test_u_file_*.py

--- tests/unit/file/test_u_file_sas7bdat.py
+++ tests/unit/file/test_u_file_sas7bdat.py
@@ -12,19 +12,16 @@ import pytest
 
 from etlplus.file import sas7bdat as mod
 from etlplus.file.base import ReadOptions
-from tests.unit.file.pytest_file_contract_contracts import (
+
+from .pytest_file_contract_contracts import (
     ReadOnlyScientificDatasetModuleContract,
 )
-from tests.unit.file.pytest_file_contract_mixins import OptionalModuleInstaller
-from tests.unit.file.pytest_file_contract_mixins import PathMixin
-from tests.unit.file.pytest_file_contract_utils import (
-    patch_dependency_resolver_unreachable,
-)
-from tests.unit.file.pytest_file_contract_utils import (
-    patch_dependency_resolver_value,
-)
-from tests.unit.file.pytest_file_support import DictRecordsFrameStub
-from tests.unit.file.pytest_file_support import PandasReadSasStub
+from .pytest_file_contract_mixins import OptionalModuleInstaller
+from .pytest_file_contract_mixins import PathMixin
+from .pytest_file_contract_utils import patch_dependency_resolver_unreachable
+from .pytest_file_contract_utils import patch_dependency_resolver_value
+from .pytest_file_support import DictRecordsFrameStub
+from .pytest_file_support import PandasReadSasStub
 
 # SECTION: TESTS ============================================================ #
 
