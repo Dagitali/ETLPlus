2b8c255f60100989b8931a8da83a771da7c4547f
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-22 22:18:26 -0500

    refactor(file): Update numbers.py

--- etlplus/file/numbers.py
+++ etlplus/file/numbers.py
@@ -16,11 +16,8 @@ Notes
 
 from __future__ import annotations
 
-from ..types import JSONData
-from ..types import JSONList
-from ..types import StrPath
-from ._io import call_deprecated_module_read
-from ._io import call_deprecated_module_write
+from ._io import make_deprecated_module_read
+from ._io import make_deprecated_module_write
 from ._stub_categories import StubSpreadsheetFileHandlerABC
 from .enums import FileFormat
 
@@ -49,10 +46,6 @@ class NumbersFile(StubSpreadsheetFileHandlerABC):
     format = FileFormat.NUMBERS
     engine_name = 'numbers'
 
-    # -- Instance Methods -- #
-
-    # Inherits read() and write() from StubSpreadsheetFileHandlerABC.
-
 
 # SECTION: INTERNAL CONSTANTS =============================================== #
 
@@ -63,52 +56,5 @@ _NUMBERS_HANDLER = NumbersFile()
 # SECTION: FUNCTIONS ======================================================== #
 
 
-def read(
-    path: StrPath,
-) -> JSONList:
-    """
-    Deprecated wrapper. Use ``NumbersFile().read(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the NUMBERS file on disk.
-
-    Returns
-    -------
-    JSONList
-        The list of dictionaries read from the NUMBERS file.
-    """
-    return call_deprecated_module_read(
-        path,
-        __name__,
-        _NUMBERS_HANDLER.read,
-    )
-
-
-def write(
-    path: StrPath,
-    data: JSONData,
-) -> int:
-    """
-    Deprecated wrapper. Use ``NumbersFile().write(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the NUMBERS file on disk.
-    data : JSONData
-        Data to write as NUMBERS file. Should be a list of dictionaries or a
-        single dictionary.
-
-    Returns
-    -------
-    int
-        The number of rows written to the NUMBERS file.
-    """
-    return call_deprecated_module_write(
-        path,
-        data,
-        __name__,
-        _NUMBERS_HANDLER.write,
-    )
+read = make_deprecated_module_read(__name__, _NUMBERS_HANDLER)
+write = make_deprecated_module_write(__name__, _NUMBERS_HANDLER)
