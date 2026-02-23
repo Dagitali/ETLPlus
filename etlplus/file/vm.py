7624615baa43575d9d4b25a47dd39a3adc5d9ca3
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-22 22:32:00 -0500

    refactor(file): Update vm.py

--- etlplus/file/vm.py
+++ etlplus/file/vm.py
@@ -20,11 +20,8 @@ from __future__ import annotations
 
 import re
 
-from ..types import JSONData
-from ..types import JSONList
-from ..types import StrPath
-from ._io import call_deprecated_module_read
-from ._io import call_deprecated_module_write
+from ._io import make_deprecated_module_read
+from ._io import make_deprecated_module_write
 from ._mixins import RegexTemplateRenderMixin
 from .base import TemplateFileHandlerABC
 from .base import TemplateTextIOMixin
@@ -94,52 +91,5 @@ _VM_HANDLER = VmFile()
 # SECTION: FUNCTIONS ======================================================== #
 
 
-def read(
-    path: StrPath,
-) -> JSONList:
-    """
-    Deprecated wrapper. Use ``VmFile().read(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the VM file on disk.
-
-    Returns
-    -------
-    JSONList
-        The list of dictionaries read from the VM file.
-    """
-    return call_deprecated_module_read(
-        path,
-        __name__,
-        _VM_HANDLER.read,
-    )
-
-
-def write(
-    path: StrPath,
-    data: JSONData,
-) -> int:
-    """
-    Deprecated wrapper. Use ``VmFile().write(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the VM file on disk.
-    data : JSONData
-        Data to write as VM file. Should be a list of dictionaries or a single
-        dictionary.
-
-    Returns
-    -------
-    int
-        The number of rows written to the VM file.
-    """
-    return call_deprecated_module_write(
-        path,
-        data,
-        __name__,
-        _VM_HANDLER.write,
-    )
+read = make_deprecated_module_read(__name__, _VM_HANDLER)
+write = make_deprecated_module_write(__name__, _VM_HANDLER)
