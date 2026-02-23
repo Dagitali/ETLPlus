be503f1b7462d1071c0ce8e2326a032f0b84a1ab
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-02-22 22:08:42 -0500

    refactor(file): Update hbs.py

--- etlplus/file/hbs.py
+++ etlplus/file/hbs.py
@@ -20,11 +20,8 @@ from __future__ import annotations
 
 import re
 
-from ..types import JSONData
-from ..types import JSONList
-from ..types import StrPath
-from ._io import call_deprecated_module_read
-from ._io import call_deprecated_module_write
+from ._io import make_deprecated_module_read
+from ._io import make_deprecated_module_write
 from ._mixins import RegexTemplateRenderMixin
 from .base import TemplateFileHandlerABC
 from .base import TemplateTextIOMixin
@@ -70,52 +67,5 @@ _HBS_HANDLER = HbsFile()
 # SECTION: FUNCTIONS ======================================================== #
 
 
-def read(
-    path: StrPath,
-) -> JSONList:
-    """
-    Deprecated wrapper. Use ``HbsFile().read(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the HBS file on disk.
-
-    Returns
-    -------
-    JSONList
-        The list of dictionaries read from the HBS file.
-    """
-    return call_deprecated_module_read(
-        path,
-        __name__,
-        _HBS_HANDLER.read,
-    )
-
-
-def write(
-    path: StrPath,
-    data: JSONData,
-) -> int:
-    """
-    Deprecated wrapper. Use ``HbsFile().write(...)`` instead.
-
-    Parameters
-    ----------
-    path : StrPath
-        Path to the HBS file on disk.
-    data : JSONData
-        Data to write as HBS file. Should be a list of dictionaries or a
-        single dictionary.
-
-    Returns
-    -------
-    int
-        The number of rows written to the HBS file.
-    """
-    return call_deprecated_module_write(
-        path,
-        data,
-        __name__,
-        _HBS_HANDLER.write,
-    )
+read = make_deprecated_module_read(__name__, _HBS_HANDLER)
+write = make_deprecated_module_write(__name__, _HBS_HANDLER)
