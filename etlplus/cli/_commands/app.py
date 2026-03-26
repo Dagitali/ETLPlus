c7822aade7f323c9be7a88c001ed0ac8ab063fdd
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-03-26 09:43:51 -0400

    refactor(cli): Create app.py

---
+++ etlplus/cli/_commands/app.py
@@ -0,0 +1,30 @@
+"""
+:mod:`etlplus.cli._commands.app` module.
+
+Typer application object for the ``etlplus`` CLI.
+"""
+
+from __future__ import annotations
+
+import typer
+
+from etlplus.cli._constants import CLI_DESCRIPTION
+from etlplus.cli._constants import CLI_EPILOG
+
+# SECTION: EXPORTS ========================================================== #
+
+
+__all__ = ['app']
+
+
+# SECTION: VARIABLES ======================================================== #
+
+
+app = typer.Typer(
+    name='etlplus',
+    help=CLI_DESCRIPTION,
+    epilog=CLI_EPILOG,
+    add_completion=True,
+    no_args_is_help=False,
+    rich_markup_mode='markdown',
+)
