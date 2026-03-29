001d3bc9d7ba05121f475f340e0bfdf19ae894c6
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-03-29 17:03:40 -0400

    test(unit): Update test_u_file_init.py

--- tests/unit/file/test_u_file_init.py
+++ tests/unit/file/test_u_file_init.py
@@ -11,6 +11,8 @@ from etlplus.file._core import File
 from etlplus.file._enums import CompressionFormat
 from etlplus.file._enums import FileFormat
 from etlplus.file._enums import infer_file_format_and_compression
+from etlplus.file.base import ReadOptions
+from etlplus.file.base import WriteOptions
 
 # SECTION: PRAGMAS ========================================================== #
 
@@ -26,6 +28,8 @@ class TestFilePackageExports:
         """Test that ``__all__`` and top-level package symbol wiring."""
         assert mod.__all__ == [
             'File',
+            'ReadOptions',
+            'WriteOptions',
             'CompressionFormat',
             'FileFormat',
             'infer_file_format_and_compression',
@@ -33,6 +37,8 @@ class TestFilePackageExports:
         assert mod.File is File
         assert mod.CompressionFormat is CompressionFormat
         assert mod.FileFormat is FileFormat
+        assert mod.ReadOptions is ReadOptions
+        assert mod.WriteOptions is WriteOptions
         assert mod.infer_file_format_and_compression is (
             infer_file_format_and_compression
         )
