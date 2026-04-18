ac3648f808e1ca4021bc6df494f32ca831e32ec1
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-04-18 12:51:39 -0400

    feat(runtime): Update __int__.py

--- etlplus/runtime/__init__.py
+++ etlplus/runtime/__init__.py
@@ -11,6 +11,11 @@ from ._events import EVENT_SCHEMA_VERSION
 from ._events import RuntimeEvents
 from ._logging import configure_logging
 from ._logging import resolve_log_level
+from ._telemetry import ResolvedTelemetryConfig
+from ._telemetry import RuntimeTelemetry
+from ._telemetry import TelemetryConfig
+from ._telemetry import configure_telemetry
+from ._telemetry import resolve_telemetry_settings
 from .readiness import ReadinessReportBuilder
 
 # SECTION: EXPORTS ========================================================== #
@@ -20,10 +25,15 @@ __all__ = [
     # Classes
     'ReadinessReportBuilder',
     'RuntimeEvents',
+    'RuntimeTelemetry',
+    'TelemetryConfig',
+    'ResolvedTelemetryConfig',
     # Constants
     'EVENT_SCHEMA',
     'EVENT_SCHEMA_VERSION',
     # Functions
     'configure_logging',
+    'configure_telemetry',
     'resolve_log_level',
+    'resolve_telemetry_settings',
 ]
