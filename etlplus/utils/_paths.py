22feaef8b7f0746fa9262290bf7f19315dfcb84c
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-05-08 13:29:06 -0400

    refactor(utils): Update _paths.py

--- etlplus/utils/_paths.py
+++ etlplus/utils/_paths.py
@@ -47,11 +47,8 @@ class PathHasher:
         path = Path(self.file_path)
         if not path.is_file():
             return None
-        digest = hashlib.sha256()
         with path.open('rb') as handle:
-            for chunk in iter(lambda: handle.read(1024 * 1024), b''):
-                digest.update(chunk)
-        return digest.hexdigest()
+            return hashlib.file_digest(handle, 'sha256').hexdigest()
 
 
 # SECTION: CLASSES ========================================================== #
