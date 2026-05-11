12ae7c58034ce30247f2f17a73db04237ba098ff
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-05-11 14:33:28 -0400

    refactor(queue): Update __init__.py

--- etlplus/queue/__init__.py
+++ etlplus/queue/__init__.py
@@ -6,20 +6,20 @@ Queue type helpers for message-oriented ETL sources and targets.
 
 from __future__ import annotations
 
+from ._amqp import AmqpQueue
+from ._amqp import AmqpQueueConfigDict
 from ._aws import AwsSqsQueue
 from ._aws import AwsSqsQueueConfigDict
+from ._azure import AzureServiceBusQueue
+from ._azure import AzureServiceBusQueueConfigDict
 from ._base import QueueConfigProtocol
 from ._enums import QueueService
 from ._enums import QueueType
+from ._gcp import GcpPubSubQueue
+from ._gcp import GcpPubSubQueueConfigDict
 from ._location import QueueLocation
-from ._providers import AmqpQueue
-from ._providers import AmqpQueueConfigDict
-from ._providers import AzureServiceBusQueue
-from ._providers import AzureServiceBusQueueConfigDict
-from ._providers import GcpPubSubQueue
-from ._providers import GcpPubSubQueueConfigDict
-from ._providers import RedisQueue
-from ._providers import RedisQueueConfigDict
+from ._redis import RedisQueue
+from ._redis import RedisQueueConfigDict
 from ._types import QueueConfig
 
 # SECTION: EXPORTS ========================================================== #
