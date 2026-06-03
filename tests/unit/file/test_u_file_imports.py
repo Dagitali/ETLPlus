51453646f86a06eb93afd01bf8d01c3f959d6c6d
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2026-06-03 18:34:35 -0400

    refactor(unit): Update test_u_file_imports.py

--- tests/unit/file/test_u_file_imports.py
+++ tests/unit/file/test_u_file_imports.py
@@ -26,23 +26,20 @@ class TestImportsHelpers:
     @pytest.mark.parametrize(
         ('method_name', 'method_args', 'expected_call'),
         [
-            pytest.param(
+            (
                 'get_pandas',
                 ('CSV',),
                 ('pandas', 'CSV', None, True),
-                id='pandas',
             ),
-            pytest.param(
+            (
                 'get_pyarrow',
                 ('PARQUET',),
                 ('pyarrow', 'PARQUET', None, True),
-                id='pyarrow',
             ),
-            pytest.param(
+            (
                 'get_yaml',
                 (),
                 ('yaml', 'YAML', 'PyYAML', True),
-                id='yaml',
             ),
         ],
     )
@@ -92,19 +89,17 @@ class TestImportsHelpers:
     @pytest.mark.parametrize(
         ('module_name', 'format_name', 'pip_name', 'dependency_name'),
         [
-            pytest.param(
+            (
                 'pyarrow',
                 'PARQUET',
                 None,
                 'pyarrow',
-                id='default_pip_name',
             ),
-            pytest.param(
+            (
                 'yaml',
                 'YAML',
                 'PyYAML',
                 'PyYAML',
-                id='explicit_pip_name',
             ),
         ],
     )
@@ -283,19 +278,17 @@ class TestImportsHelpers:
     @pytest.mark.parametrize(
         ('module_name', 'format_name', 'pip_name', 'dependency_name'),
         [
-            pytest.param(
+            (
                 'duckdb',
                 'DUCKDB',
                 None,
                 'duckdb',
-                id='default_pip_name',
             ),
-            pytest.param(
+            (
                 'bson',
                 'BSON',
                 'pymongo',
                 'pymongo',
-                id='explicit_pip_name',
             ),
         ],
     )
